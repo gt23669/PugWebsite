31f64d8460c3c0c54f3aaa23679ad8008c119b7d
Dcorum <dpcorum@yahoo.com>
2018-05-23 16:50:59 -0600

    updated code to pull information from jsonOB correctly for each page aside from index

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,181 +1,52 @@
 
 var express = require('express');
+var fs = require('fs');
 
 var router = express.Router();
 
+var jsonOB = JSON.parse(fs.readFileSync("./src/data/data.json", "utf8"));
+
 router.route("/").get(function (request, response) {
     // response.sendFile(__dirname + "/views/index.html");
-    var data = { title: "Index Page", h1Text: "Neumont Kangaroos Kickball Team", contactus: "Contact Us" };
+    var data = {
+        title: "Index Page",
+        h1Text: "Neumont Kangaroos Kickball Team",
+        contactus: "Contact Us"
+    };
     response.render('index', data);
 
 }
 );
 router.route("/roster").get(function (request, response) {
-    // response.sendFile(__dirname + "/views/index.html");
+    // var jsonOB = JSON.parse(fs.readFileSync("./src/data/data.json", "utf8"));
     var data = {
-        title: "Team Roster", h1Text: "Roster", roster: [{
-            image:"/css/images/Names.png",
-            name: "Name",
-            position: "Position",
-            number: "Number",
-            skill: "Special Skill"
-        }, {
-            image: "/css/images/FoxJersey.png",
-            name: "Mr. Fox",
-            position: "Catcher",
-            number: 0,
-            skill: "Creates a water droplet sound with mouth."
-        }, {
-            image: "/css/images/Blank.png",
-            name: "Juan",
-            position: "Outfield Left",
-            number: 1+" (lol)",
-            skill: "Can fold fingers behind each other"
-        }, {
-            image: "/css/images/Blank.png",
-            name: "Daniel",
-            position: "Outfield Right",
-            number: 2,
-            skill: "Wiggle Ears"
-        }, {
-            image: "/css/images/KrebsJersey.png",
-            name: "Mr. Krebs",
-            position: "2nd Base",
-            number: 3,
-            skill: "Competion Ballroom Dancer"
-        }, {
-            image: "/css/images/KohlerJersey.png",
-            name: "Dr. Kohler",
-            position: "OutField Center",
-            number: 4,
-            skill: "6' Wing Span"
-        }, {
-            image: "/css/images/ThompsonJersey.png",
-            name: "Mrs. Thompson",
-            position: "Blank Position field",
-            number: 5,
-            skill: "Blank Skill Field"
-        }, {
-            image: "/css/images/WarnerJersey.png",
-            name: "Mr. Warner",
-            position: "3rd Base",
-            number: 6,
-            skill: "Can sing in 2 tones"
-        }, {
-            image: "/css/images/DennisJersey.png",
-            name: "Dennis",
-            position: "Bench",
-            number: 7,
-            skill: "Can twist hand 360° on a table"
-        }, {
-            image: "/css/images/Blank.png",
-            name: "Mr. Fletcher",
-            position: "Pitcher",
-            number: 8,
-            skill: "Speakes loudly"
-        }, {
-            image: "/css/images/BenJersey.png",
-            name: "Ben",
-            position: "Right Bench",
-            number: 9,
-            skill: "Writes Music"
-        }, {
-            image: "/css/images/Blank.png",
-            name: "Jorden",
-            position: "Shortstop Left",
-            number: 10,
-            skill: "Chess"
-        }, {
-            image: "/css/images/ShaneJersey.png",
-            name: "Shane",
-            position: "1st Base",
-            number: 11,
-            skill: "Musician"
-        }, {
-            image: "/css/images/Blank.png",
-            name: "Mr. Harrison",
-            position: "Empty Position Field",
-            number: 12,
-            skill: "Empty Skill Field"
-        }, {
-            image: "/css/images/CoxJersey.png",
-            name: "Mr. Cox",
-            position: "Played water polo",
-            number: 13,
-            skill: "Empty Skill Field"
-        }, {
-            image: "/css/images/Blank.png",
-            name: "JP",
-            position: "Empty Position Field",
-            number: 14,
-            skill: "Empty Skill Field"
-        }]
+        title: "Team Roster",
+        h1Text: "Roster",
+        members: jsonOB.roster
     };
+    console.log(jsonOB.roster);
     response.render('roster', data);
 
 }
 );
 router.route("/records").get(function (request, response) {
-    // response.sendFile(__dirname + "/views/index.html");
+    // var jsonOB = JSON.parse(fs.readFileSync("./src/data/data.json", "utf8"));
     var data = {
-        title: "Team Records", h1Text: "Previous Seasons", records: [{
-            year: "2010",
-            record: "Home:'20' Visiting:'10'"
-        }, {
-            year: "2011",
-            record: "Home:'25' Visiting:'5'"
-        }, {
-            year: "2012",
-            record: "Home:'14' Visiting:'3'"
-        }, {
-            year: "2013",
-            record: "Home:'18' Visiting:'16'"
-        }, {
-            year: "2014",
-            record: "Home:'8' Visiting:'11'"
-        }, {
-            year: "2015",
-            record: "Home:'21' Visiting:'10'"
-        }, {
-            year: "2016",
-            record: "Home:'9' Visiting:'15'"
-        }, {
-            year: "2017",
-            record: "Home:'10' Visiting:'11'"
-        }]
+        title: "Team Records",
+        h1Text: "Previous Seasons",
+        records: jsonOB.records
     };
     response.render('records', data);
 
 }
 );
 router.route("/orders").get(function (request, response) {
-    // response.sendFile(__dirname + "/views/index.html");
-    var data = { title: "Orders Page", h1Text: "Team Orders Page", items:[{
-        // image:"/css/images/Blank.png",
-        image:"/css/images/bagSales.png",
-        item:"Bags",
-        price:1.00
-    },
-    {
-        image:"/css/images/Blank.png",
-        item:"Shirt",
-        price:2.00
-    },
-    {
-        image:"/css/images/Blank.png",
-        item:"Shoes",
-        price:3.00
-    },
-    {
-        image:"/css/images/Blank.png",
-        item:"Gloves",
-        price:4.00
-    },
-    {
-        image:"/css/images/Blank.png",
-        item:"Pants",
-        price:5.00
-    },] };
+    // var jsonOB = JSON.parse(fs.readFileSync("./src/data/data.json", "utf8"));
+    var data = {
+        title: "Orders Page",
+        h1Text: "Team Orders Page",
+        items: jsonOB.items
+    };
     response.render('orders', data);
 
 }
@@ -187,23 +58,7 @@ router.route("/contactUs").get(
         var model = {
             title: "Contact Us Page",
             h1Text: "Contact Us Here!",
-            contactData: [
-                {
-                    name: "Juan",
-                    phone: "123-456-7890",
-                    email: "Jtaguilar@student.neumont.edu"
-                },
-                {
-                    name: "DCorum",
-                    phone: "098-765-4321",
-                    email: "Dcorum@student.neumont.edu"
-                },
-                {
-                    name: "NCKickballHQ",
-                    phone: "555-555-5555",
-                    email: "NCKickballHQ@neumont.edu"
-                }
-            ]
+            contactData: jsonOB.contactData
 
         };
         response.render("contactUs", model);
